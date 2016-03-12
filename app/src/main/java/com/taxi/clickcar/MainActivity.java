473194dedf2c7a33a5be883212c6d60c2a54d999
wwoodpacker <woodpacker370@gmail.com>
2016-03-12 17:18:14 +0200

    Intro working +- design - first connect to server sha512+

--- app/src/main/java/com/taxi/clickcar/MainActivity.java
+++ app/src/main/java/com/taxi/clickcar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.taxi.clickcar;
 
+import android.accounts.NetworkErrorException;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.Xml;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,21 +32,47 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-
+public static final String ADRESS="http://176.38.246.10:7200";
+public static final String AUTH_URL=ADRESS+"/api/account";
+public static final String CONFIRM_CODE=ADRESS+"/api/account/register/sendConfirmCode";
+public static final String REGISTER=ADRESS+"/api/account/register";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        my task = new my();
-        task.execute();
+      //  my task = new my();
+       // task.execute();
+        try {
+            String sha512=hashText("1");
+            Log.e("sha512",sha512);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static String convertByteToHex(byte data[])
+    {
+        StringBuffer hexData = new StringBuffer();
+        for (int byteIndex = 0; byteIndex < data.length; byteIndex++)
+            hexData.append(Integer.toString((data[byteIndex] & 0xff) + 0x100, 16).substring(1));
+
+        return hexData.toString();
     }
 
+    public static String hashText(String textToHash) throws Exception
+    {
+        final MessageDigest sha512 = MessageDigest.getInstance("SHA-512");
+        sha512.update(textToHash.getBytes());
 
+        return convertByteToHex(sha512.digest());
+    }
  class my extends AsyncTask<Void,Void,Void>{
 
      @Override
@@ -64,6 +92,8 @@ public class MainActivity extends AppCompatActivity {
              response = client.execute(post);
          } catch (IOException e) {
              e.printStackTrace();
+         }catch (NullPointerException ex){
+
          }
          String text="";
          HttpEntity entity = null;
