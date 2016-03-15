caeb98739890369b8be9d75ded65dadf10438bcc
wwoodpacker <woodpacker370@gmail.com>
2016-03-15 05:07:43 +0200

    Intro working + design -+ first connect to server sha512+

--- app/src/main/java/com/taxi/clickcar/Intro/ClickIntro.java
+++ app/src/main/java/com/taxi/clickcar/Intro/ClickIntro.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 import com.github.paolorotolo.appintro.AppIntro2;
 import com.taxi.clickcar.MainActivity;
@@ -27,7 +28,9 @@ public class ClickIntro extends AppIntro2 {
 
         addSlide(ClickSlide.newInstance(R.layout.intro));
         addSlide(ClickSlide.newInstance(R.layout.intro2));
+        addSlide(ClickSlide.newInstance(R.layout.intro3));
         setFadeAnimation();
+        setIndicatorColor(R.color.process_active, R.color.process_notactive);
     }
 
 
